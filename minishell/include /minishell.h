5790a93ac549fcf6e33c4ced31d73d55cc6e4199
Mohammed Abrar Shariar <mshariar@k1r1p1.42mulhouse.fr>
2025-05-11 02:42:41 +0200

    start

---
+++ minishell/include /minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshariar <mshariar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/11 02:38:31 by mshariar          #+#    #+#             */
+/*   Updated: 2025/05/11 02:38:34 by mshariar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+
+
+
+
+#endif
